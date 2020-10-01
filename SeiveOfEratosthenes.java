1f3319250b0078d922857da245dbf265850c149f
Shruti Singhal <shruti29s06@gmail.com>
2020-10-01 20:24:11 +0530

    Seive of Eratosthenes

---
+++ SeiveOfEratosthenes.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class SeiveOfEratosthenes
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		boolean prime[] = new boolean[n+1];
+		for(int i=0;i<n;i++){
+			prime[i] = true;
+		}
+		for(int p=2;p*p<=n;p++){
+			if(prime[p]==true){
+				for(int i=p*p;i<=n;i+=p){
+					prime[i] = false;
+				}
+			}
+		}
+		for(int i=2;i<=n;i++){
+			if(prime[i]==true){
+				System.out.print(i+" ");
+			}
+		}
+	}
+}
