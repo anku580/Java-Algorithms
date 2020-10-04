a93d082a97c036ccc4c54823f1d7997a153dda97
BrijeshSinghRawat <123789b@gmail.com>
2020-10-04 16:51:54 +0530

    Improving the Performance of Bubble Sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,10 +1,14 @@
 // Java program for implementation of Bubble Sort
-class BubbleSort
+
+public class BubbleSort
 {
 	void bubbleSort(int arr[])
 	{
 		int n = arr.length;
-		for (int i = 0; i < n-1; i++)
+		boolean swapped;
+		for (int i = 0; i < n-1; i++){
+		printArray(arr);
+		swapped=false;
 			for (int j = 0; j < n-i-1; j++)
 				if (arr[j] > arr[j+1])
 				{
@@ -12,7 +16,12 @@ class BubbleSort
 					int temp = arr[j];
 					arr[j] = arr[j+1];
 					arr[j+1] = temp;
+					swapped=true;
 				}
+		if(!swapped){
+		    break;
+		}
+		}
 	}
 
 	/* Prints the array */
@@ -28,10 +37,9 @@ class BubbleSort
 	public static void main(String args[])
 	{
 		BubbleSort ob = new BubbleSort();
-		int arr[] = {64, 34, 25, 12, 22, 11, 90};
+		int arr[] = {5, 2, 9, 1, 7, 3, 4, 505, 200};
 		ob.bubbleSort(arr);
 		System.out.println("Sorted array");
 		ob.printArray(arr);
 	}
 }
-/* This code is contributed by Rajat Mishra */
