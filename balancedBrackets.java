9c05347cd8980173b6f05e5e8c2b31043a286ced
mohammadasad9450 <68806812+mohammadasad9450@users.noreply.github.com>
2020-10-04 22:49:49 +0530

    Update balancedBrackets.java

--- balancedBrackets.java
+++ balancedBrackets.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
+public class balancedBrackets {
 
     // Complete the isBalanced function below.
     static String isBalanced(String s) throws Exception {
