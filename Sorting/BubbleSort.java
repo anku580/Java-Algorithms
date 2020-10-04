9b9947d1c00c63fecf06674886e8f9752e6e3e26
BrijeshSinghRawat <123789b@gmail.com>
2020-10-04 16:53:56 +0530

    Improving the Performance of Bubble sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -7,7 +7,6 @@ public class BubbleSort
 		int n = arr.length;
 		boolean swapped;
 		for (int i = 0; i < n-1; i++){
-		printArray(arr);
 		swapped=false;
 			for (int j = 0; j < n-i-1; j++)
 				if (arr[j] > arr[j+1])
